9bf9464178a5ac1914499154358f79813c6fbab5
Hiroshi McBride <roninnarasu@gmail.com>
2024-04-24 13:37:33 +0200

    add packable class

--- src/box/box_drop_detection.gd
+++ src/box/box_drop_detection.gd
@@ -2,7 +2,12 @@ class_name BoxItemDetection extends Area3D
 
 func _enter_tree():
 	body_entered.connect(_on_item_entered)
+	body_entered.connect(_on_item_exited)
 
 func _on_item_entered(body):
 	if body is Packable:
-		pass
+		body.set_box_detected(true)
+
+func _on_item_exited(body):
+	if body is Packable:
+		body.set_box_detected(false)
