5b3c6561b39700a587ccd3fb46b85bd2b003b3e8
Nina Buurman <nina.buurman@student.hku.nl>
2024-05-09 12:01:25 +0200

    Item depositing requires an input (scuffed)
    *player has to press interact button to deposit item, however the item still gets deposited when an input is detected even after it has exited the collider of the box

--- src/props/box_item_detection.gd
+++ src/props/box_item_detection.gd
@@ -5,6 +5,10 @@ class_name BoxItemDetection extends Area3D
 var item_count : int
 var max_items : int
 
+var item_detected : bool
+
+var item = null
+
 func _enter_tree():
 	body_entered.connect(_on_item_entered)
 	body_entered.connect(_on_item_exited)
@@ -13,12 +17,20 @@ func _ready():
 	max_items = contents.get_children().size()
 
 func _on_item_entered(body):
-	if body is Packable and item_count < max_items: #add that the player should press the interact button (+prompt "deposit in box")
-			body.pack(self)
-			var i = contents.get_child(item_count) as Node3D
-			i.visible = true
-			item_count += 1
+	#item_detected = true
+	item = body
 
 func _on_item_exited(body):
+	#item_detected = false
 	if body is Packable:
 		pass
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("interact"):
+		if item != null:
+			if item is Packable and item_count < max_items:  #!! items still get packed even if they move out of the collider !!
+				item.pack(self)
+				var i = contents.get_child(item_count) as Node3D
+				i.visible = true
+				item_count += 1
+				item = null
