cfd6043853c3bc5afa134e8d5c9b18b8bfbbd31f
Nina Buurman <nina.buurman@student.hku.nl>
2024-08-21 17:00:39 +0200

    Holdable objects are static until picked up
    *needs testing & bug fixing

--- src/props/holdable.gd
+++ src/props/holdable.gd
@@ -8,11 +8,13 @@ signal dropped()
 func _ready():
 	can_sleep = false
 	freeze_mode = RigidBody3D.FREEZE_MODE_STATIC
+	freeze = true
 	picked.connect(_on_picked)
 	dropped.connect(_on_dropped)
 
 func _on_picked():
 	is_held = true
+	freeze = false
 
 func _on_dropped():
 	is_held = false
